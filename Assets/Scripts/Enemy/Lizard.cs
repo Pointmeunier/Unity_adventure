4c3bd8c1aa530186e52ab39223fa9b3dcec58d02
Pointmeunier <garry70126@gmail.com>
2024-12-21 00:49:24 +0800

    Update 綠水龍

--- Assets/Scripts/Enemy/Lizard.cs
+++ Assets/Scripts/Enemy/Lizard.cs
@@ -3,15 +3,6 @@ using UnityEngine;
 
 public class Lizard : Enemy
 {
-    Character character;
-
-    private void Start()
-    {
-        // 獲取 Character 元件
-        character = GetComponent<Character>();
-    }
-
-
     public override void Move()
     {
         base.Move();
