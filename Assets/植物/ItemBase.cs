b3e6838f275015ed9d13c6091b2374cc447f68a7
nemoooo123 <lovesimple039@gmail.com>
2024-12-26 16:49:54 +0800

    1226植物音效
    LAI

--- Assets/植物/ItemBase.cs
+++ Assets/植物/ItemBase.cs
@@ -25,6 +25,14 @@ public class ItemBase : MonoBehaviour
     }
 
     public ItemType itemType; // 物品类型
+    
+    public AudioClip boostSound;  // 增益音效
+    public AudioClip debuffSound; // 減益音效
+    private PlayerController playerController;
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>(); // 獲取玩家控制器
+    }
 
     public void ApplyEffect(PlayerController player)
     {
@@ -51,48 +59,58 @@ public class ItemBase : MonoBehaviour
             case ItemType.AttackBoost:
                 player.IncreaseAttack(5);
                 // 销毁物品
+                player.PlayBoostSound();  // 播放增益音效
                 Destroy(gameObject);
                 break;
             case ItemType.Attackreduce:
                 player.DecreaseAttack(5);
                 // 销毁物品
+                player.PlayDebuffSound();  // 播放減益音效
                 Destroy(gameObject);
                 break;
             case ItemType.HealthRestore:
                 player.RestoreHealth(50f);
                 // 销毁物品
+                player.PlayBoostSound();  // 播放增益音效
                 Destroy(gameObject);
                 break;
             //10趴血
             case ItemType.HealthRestore10percent:
                 player.RestoreHealthtenpercent(1.1f);
+                player.PlayBoostSound();  // 播放增益音效
                 // 销毁物品
                 Destroy(gameObject);
                 break;
             //扣一半血
             case ItemType.Healthreducepercent:
                 player.Healthreducepercent(0.5f);
+                player.PlayDebuffSound();  // 播放減益音效
                 // 销毁物品
                 Destroy(gameObject);
                 break;
                 //扣50血
             case ItemType.Healthreduce:
                 player.Healthreduce(50f);
+                player.PlayDebuffSound();  // 播放減益音效
                 // 销毁物品
                 Destroy(gameObject);
                 break;
             
             case ItemType.SpeedBoost:
+                player.PlayBoostSound();  // 播放增益音效
                 player.BoostSpeed(1.5f); // 速度每次提升50%3秒
                 
                 break;
             //減速
             case ItemType.SpeedReduce:
+                player.PlayDebuffSound();  // 播放減益音效
                 player.SpeedReduce(0.5f);  
                 
                 break;
             case ItemType.JumpBoost:
                 player.BoostJump(1.3f); // 每次提升30%
+                
+                player.PlayBoostSound();  // 播放增益音效
                 
                 break;
         }
